095af57729897c4be1258c660adea31f9d3e256b
manish.patel.mnnit@gmail.com <ec2-user@ip-172-31-23-55.us-east-2.compute.internal>
2018-11-06 05:38:09 +0000

    signup added

--- db/schema.rb
+++ db/schema.rb
@@ -50,14 +50,4 @@ ActiveRecord::Schema.define(version: 20180425010649) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "firstname"
-    t.string "lastname"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.string "remember_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
 end

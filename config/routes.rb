10c750089568dd4c6133bbaf428918d653c53770
manish.patel.mnnit@gmail.com <ec2-user@ip-172-31-23-55.us-east-2.compute.internal>
2018-11-16 22:55:37 +0000

    changed route

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  match 'users/show',   to: "trips#index",      via: :get
   # Edit end
   resources :users
 end

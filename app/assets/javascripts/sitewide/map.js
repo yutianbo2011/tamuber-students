6aa2780c7e4db7f8ee84f970dd55f8208a5e1652
EC2 Default User <ec2-user@ip-172-31-6-17.us-east-2.compute.internal>
2018-11-18 05:02:23 +0000

    log more

--- app/assets/javascripts/sitewide/map.js
+++ app/assets/javascripts/sitewide/map.js
@@ -36,7 +36,7 @@ function initMapWithMarker(lat, lng, startPoint) {
       var start = [-96.3409565,30.6189768];
       var end = [ -96.3425741,30.6213251];
       console.log("travel time invoked from outside");
-      getTravelTime(start[1], start[0], end[1], end[0]);
+      // getTravelTime(start[1], start[0], end[1], end[0]);
       
       map.on('load', function() {
         getRoute(start,end);
@@ -259,26 +259,27 @@ function selectRoute(route) {
 	$('#selectedRoute').text(route);
 }
 
-// function getCartLiveLoc(){
+// function getCartLiveLoc(){q
   
 // }
 
-function getTravelTime(startGPSLat, startGPSLon,endGPSLat, endGPSLon){
-  console.log("travel time invoked")
-  startGPS = [startGPSLat,startGPSLon];
-  console.log("travel time invoked2")
-  endGPS = [endGPSLat,endGPSLon];
-  console.log("travel time invoked3")
-  listOfPoints = startGPS + ";" + endGPS;
-  console.log("travel time invoked4")
-  var directionsMatrixClient = MapboxMatrix.builder()
-    .accessToken('pk.eyJ1IjoiZ3Vsc2hhbmsiLCJhIjoiY2pvM3d1NGV3MTFydzN3cWlkZ2xjdmE1MSJ9.zQ1AATk2EOGJ4XMDyBV9vA')
-    .profile(DirectionsCriteria.PROFILE_DRIVING)
-    .coordinates(listOfPoints)
-    .build()
+// function getTravelTime(startGPSLat, startGPSLon,endGPSLat, endGPSLon){
+//   console.log("travel time invoked")
+//   startGPS = [startGPSLat,startGPSLon];
+//   console.log("travel time invoked2")
+//   endGPS = [endGPSLat,endGPSLon];
+//   console.log("travel time invoked3")
+//   listOfPoints = startGPS + ";" + endGPS;
+//   pointList = new ArrayList<>();
+//   console.log("travel time invoked4")
+//   var directionsMatrixClient = MapboxMatrix.builder()
+//     .accessToken('pk.eyJ1IjoiZ3Vsc2hhbmsiLCJhIjoiY2pvM3d1NGV3MTFydzN3cWlkZ2xjdmE1MSJ9.zQ1AATk2EOGJ4XMDyBV9vA')
+//     .profile(DirectionsCriteria.PROFILE_DRIVING)
+//     .coordinates(listOfPoints)
+//     .build()
        
-    //console.log( "My travel times are:", directionsMatrixClient[0][0], directionsMatrixClient[0][1], directionsMatrixClient[1][0], directionsMatrixClient[1][1])
-}
+//     //console.log( "My travel times are:", directionsMatrixClient[0][0], directionsMatrixClient[0][1], directionsMatrixClient[1][0], directionsMatrixClient[1][1])
+// }
 
 // // function calculateDistance(strtLat, strtLong, endLat, endLong){
  

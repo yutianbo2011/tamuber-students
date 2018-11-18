750b4371a015db255efb7ee536f42c7ea18c58c8
manish.patel.mnnit@gmail.com <ec2-user@ip-172-31-23-55.us-east-2.compute.internal>
2018-11-18 05:34:52 +0000

    chang

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,14 @@ class ApplicationController < ActionController::Base
   
   # Added the following for Login 
   # Edit Manish start
+  before_filter :check_token
+  
   include SessionsHelper
+  def check_token
+    unless logged_in?
+      session[:original_url] = request.url
+      redirect_to :controller => :users, :action => :new
+    end
+  end
   # Edit Manish end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  skip_before_filter :check_token
+  
   def show
     @user = User.find(params[:id])
   end

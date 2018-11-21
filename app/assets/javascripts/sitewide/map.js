30913f9aefb9b501db20e9774e271bf3963bc64a
EC2 Default User <ec2-user@ip-172-31-6-17.us-east-2.compute.internal>
2018-11-21 08:01:13 +0000

    maps api test

--- app/assets/javascripts/sitewide/map.js
+++ app/assets/javascripts/sitewide/map.js
@@ -370,7 +370,8 @@ function updateRoute(source, destination) {
   var newCoords = source[0]+','+source[1]+';'+destination[0]+','+destination[1];
    console.log("newCoords are " + newCoords);
   //var newCoords = "-96.340379,30.620167;-96.323706,30.609521";
-  getMatch(newCoords);
+   setTimeout(getMatch(newCoords), 10000);
+  //getMatch(newCoords);
 }
 
 function removeRoute () {
@@ -410,54 +411,57 @@ function getMatch(e) {
     // https://www.mapbox.com/api-documentation/#directions
     console.log("match route invoked");
     mapboxgl.accessToken = ACCESS_TOKEN
-    var url = 'https://api.mapbox.com/directions/v5/mapbox/driving/' + e +'?geometries=geojson&steps=true&&access_token=' + mapboxgl.accessToken;
+    var directionsRequest = 'https://api.mapbox.com/directions/v5/mapbox/driving/' + e +'?geometries=geojson&steps=true&&access_token=' + mapboxgl.accessToken;
+    
+    $.ajax({
+          method: 'GET',
+          url: directionsRequest,
+        }).done(function(data) {
+            var geo = data.routes[0].geometry;
+            addRoute(geo);
+            var distancebtw = data.routes[0].distance*0.001;
+            var durationbtw = data.routes[0].duration*60;
+            
+            console.log("distance is " + distancebtw);
+            console.log("duration is " + durationbtw);
+            
+            var route = geo;
+            if(geo!=null && geo.coordinates.length!=0){
+            console.log("data " +geo);
+            console.log("data " + geo.coordinates[0])
+            console.log("fsdfsd"+geo.coordinates.length);
+            console.log("travel time invoked from outside");
+            
+            start = geo.coordinates[0];
+            end = geo.coordinates[geo.coordinates.length-1];
+            console.log(route);
+            setTimeout(getMatch(e), 10000);
+          }
+        })
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
     var req = new XMLHttpRequest();
     req.responseType = 'json';
     req.open('GET', url, true);
     console.log('received json data ' + req.response)
     
-    /*
-    req.onreadystatechange = function() {
-     switch( req.readyState ) {
-          case 4: {
-              if (req.status === 200) {
-                  var txt = req.responseText; // or do something else here
-                  addRoute(coords);
-              }
-              break;
-          }
-          case 3: {
-              // go interactive !
-              break;
-          }
-     }
-};
-*/
-      
-      
-     
-/*req.onreadystatechange = function () {
-    var DONE = this.DONE || 4;
-    var jsonResponse = req.response;
-      var distance = jsonResponse.routes[0].distance*0.001*0.621371; // convert to km
-      var duration = jsonResponse.routes[0].duration/60; // convert to minutes
-      console.log("distance is " + distance)
-      console.log("duration is " + duration)
-      // add results to info box
-      document.getElementById('calculated-line').innerHTML = 'Distance: ' + distance.toFixed(2) + ' mi<br>Duration: ' + duration.toFixed(2) + ' minutes';
-      var coords = jsonResponse.routes[0].geometry;
-    if (this.readyState === DONE){
-      addRoute(coords);  
-    }
-};    
-req.send();*/
-      
-    
-    
-    
-    
-    
-    
+   
     req.onload  = function() {
       var jsonResponse = req.response;
       var distance = jsonResponse.routes[0].distance*0.001*0.621371; // convert to km
@@ -478,7 +482,7 @@ req.send();*/
         console.log("timeout");
       }, 10000);
     
-}
+}*/
 
 
 function addRoute (coords) {

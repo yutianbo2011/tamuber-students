1a52149656af1caed205ff54b9f7d553d326d541
EC2 Default User <ec2-user@ip-172-31-6-17.us-east-2.compute.internal>
2018-11-22 03:41:38 +0000

    repeat

--- app/assets/javascripts/sitewide/map.js
+++ app/assets/javascripts/sitewide/map.js
@@ -35,7 +35,7 @@ function initMapWithMarker(start, end) {
         style: 'mapbox://styles/mapbox/streets-v9', // style URL
         center: start,//[-96.3365,30.6185],
         //center: //[lat,lng], // starting position as [lng, lat]
-        zoom: 10
+        zoom: 13
       });
       
       //var start = [-96.340379, 30.620167]

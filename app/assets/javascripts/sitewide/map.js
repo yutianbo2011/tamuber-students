2bdcb52b26f11ee8b7f1c3dd4a62dfda5d94379c
EC2 Default User <ec2-user@ip-172-31-6-17.us-east-2.compute.internal>
2018-11-23 03:14:20 +0000

    repeat

--- app/assets/javascripts/sitewide/map.js
+++ app/assets/javascripts/sitewide/map.js
@@ -204,7 +204,7 @@ function initMapWithMarker(start, end, liveLocation) {
           var markerLive = new mapboxgl.Marker(liveDiv)
                 .setLngLat(liveLocation)
           // markerLive.feature.anchor("[15,15]"); 
-          // markerLive.addTo(map);
+          markerLive.addTo(map);
           
         }).always(function(){
                     // map.addLayer({

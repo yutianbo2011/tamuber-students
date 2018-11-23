73b628cef9ba43db40b5d3f0a35f68e972924e21
EC2 Default User <ec2-user@ip-172-31-6-17.us-east-2.compute.internal>
2018-11-23 03:57:49 +0000

    repeat

--- app/assets/javascripts/sitewide/map.js
+++ app/assets/javascripts/sitewide/map.js
@@ -246,7 +246,7 @@ function initMapWithMarker(start, end, liveLocation) {
       }
       setInterval(function(){
         console.log("Hello");
-        if(markerLive!= null && stepSize>=route.coordinates.length){
+        if(markerLive!= null && stepSize<=route.coordinates.length){
           markerLive.setLngLat(route.coordinates[stepSize++]);
           console.log("Change");
         }

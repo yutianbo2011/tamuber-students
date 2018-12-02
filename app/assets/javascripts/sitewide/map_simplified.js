8343acde5e80439e8aad29e70f9dc35d35749c48
EC2 Default User <ec2-user@ip-172-31-6-17.us-east-2.compute.internal>
2018-12-02 09:55:31 +0000

    repeat

--- app/assets/javascripts/sitewide/map_simplified.js
+++ app/assets/javascripts/sitewide/map_simplified.js
@@ -36,14 +36,15 @@ function findMidPoint(start,end) {
 }
 
 function initMapMarkerCart2(start,end,vehicleId){
-    var fetchLiveUrl = 'https://api.myjson.com/bins/o0td6';
+    // var fetchLiveUrl = 'https://api.myjson.com/bins/o0td6';
     // // 'https://jsonbin.io/5c03821b1deea01014bbb72f';
     // //'http://tamuber-mock-server.herokuapp.com/api/vehicles/'+vehicleId;
-    // var fetchLiveUrl = 'https://raw.githubusercontent.com/rohan54/tamuber-students/master/myjson.json';
-    $.ajax({
-        method: 'GET',
-        url: fetchLiveUrl,
-    }).done(function(vehicle) {
+    var fetchLiveUrl = 'https://raw.githubusercontent.com/rohan54/tamuber-students/master/myjson.json';
+    // $.ajax({
+    //     method: 'GET',
+    //     url: fetchLiveUrl,
+    // }).done(function(vehicle) {
+    $.getJSON(fetchLiveUrl, function(vehicle) {
         // vehicle = JSON.parse(vehicle);
         console.log("vehicle");
         console.log(vehicle);

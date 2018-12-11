19bab0c8063848b64fe01c568d639196bba51b8b
PrakharMohanAgarwal <prakharmohan699@gmail.com>
2018-12-11 07:57:46 +0000

    Working things

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,97 +4,6 @@ class TripsController < ApplicationController
         @cartRoutes = CartRoute.all
     end
     
-    # def new
-    #     #cutoff = DateTime.current - 5.minutes
-    #     cutoff = DateTime.current - 1.seconds
-    #     Cart.all.each do |cart|
-    #         if (cart.inUse)
-    #             if(cart.last_busy_check)
-    #                 if (cart.last_busy_check < cutoff)
-    #                     cart.inUse = false
-    #                     cart.save
-    #                 end
-    #             else
-    #                 cart.inUse = false
-    #                 cart.save
-    #             end
-    #         end
-    #     end
-        
-    #     max_seats = 6
-    #     if params.has_key?(:seat_count)
-    #         min = params[:seat_count].to_i
-    #         if (min < 1)
-    #             min = 1
-    #         end
-    #       seats = [min .. max_seats] 
-    #     else
-    #         seats = [1..max_seats]
-    #     end
-        
-    #     if params.has_key?(:handicap_access)
-    #         needs_assist = params[:handicap_access].to_s == "on"
-    #     else
-    #         needs_assist = false
-    #     end
-        
-    #     if needs_assist
-    #         @cartRoutes = CartRoute.joins(:cart).where(:carts => {:inUse => false}).where(:carts => {:seat_count => seats}).where(:carts => {:handicap_access => true})
-    #     else
-    #         @cartRoutes = CartRoute.joins(:cart).where(:carts => {:inUse => false}).where(:carts => {:seat_count => seats})
-    #     end
-        
-    #     @trip = Trip.new
-    #     @trip.save
-    #     routeDataStr = params[:routeData].to_s
-    #     start = routeDataStr.split('startPoint')
-        
-    #     #redirect to specify if we don't have route data, otherwise act normally
-    #     if (params[:routeData].class == 'string'.class)
-            
-    #         #filter routes by seat number and availability
-    #         seatcount = params[:seat_count].to_i
-    #         carts = Cart.where('seat_count >= ?', seatcount).where(:inUse => false).as_json
-            
-    #         #filter by handicap access
-    #         if (params[:handicap_access]) 
-    #             carts = carts.keep_if{ |h| h['handicap_access'] == true}
-    #         end
-            
-            
-    #         #filter routes by available carts
-    #         if(params[:routeData] != "") 
-    #             routeDataHash = JSON.parse params[:routeData]
-    #         else 
-    #             routeDataHash = Array.new
-    #         end
-    #         routesWithAvailCarts = routeDataHash.keep_if do |el|
-    #             cartAvail = false
-    #             carts.each do |cart|
-    #               if el['cartID'].to_i == cart['id']
-    #                   cartAvail = true
-    #                   break
-    #               end 
-    #             end
-    #             cartAvail 
-    #         end
-            
-            
-             
-    #         #remove duplicate routes
-    #         @routeData = routesWithAvailCarts.uniq{ |s| s.values_at('startPoint', 'endPoint') }
-    #         if @routeData.length == 0
-    #             flash[:alert] = 'No carts available with those specifications'
-    #             redirect_to '/specify'
-    #         end
-            
-    #         @trip = Trip.new
-    #         @trip.save
-    #     else
-    #         redirect_to '/specify'
-    #     end
-    # end
-    
     def new
         # print params
         warn_string = ""
@@ -141,17 +50,7 @@ class TripsController < ApplicationController
         # find source and destination dropoff
         @source = Location.find_by name: @src
         @destination = Location.find_by name: @dest
-        
-        # if @source == nil
-         
     end
-    
-    # def specify
-    #     #Dummy ips for testing the ros functions to get route data
-    #     @cartIPs = Cart.all.select(:IP)
-    #     @cartIDs = Cart.all.select(:id)
-    #     @message = flash[:alert]
-    # end
     
     def specify
         # @places = ["ETB", "HRBB", "MSC", "Evans Library", "Kyle Field", "REC",
